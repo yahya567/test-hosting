234a82b6a7b430ac1bc7bb92a093b656b01beb42
Yahya Mohammed <yahiyamohammed455@gmail.com>
2026-02-08 17:30:28 +0300

    Added password rest to mediacms

--- addis/reset.php
+++ addis/reset.php
@@ -1,6 +1,15 @@
 <?php
 require 'admin_api.php';
 
+// error_log all application/json request to this endpoint for debugging
+error_log("=== RESET PASSWORD REQUEST ===");
+error_log("REQUEST METHOD: " . $_SERVER['REQUEST_METHOD']);
+error_log("REQUEST URI: " . $_SERVER['REQUEST_URI']);
+error_log("REQUEST HEADERS: " . json_encode(getallheaders()));
+error_log("REQUEST BODY: " . file_get_contents('php://input'));
+error_log("==============================");
+
+
 $phone = $_GET['phone'] ?? '';
 $newPassword = $_GET['new_password'] ?? '';
 $comm = $_GET['comm'] ?? '';
